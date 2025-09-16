44356868653822243729e7b859eeb44b62b44c90
Vishal Dixit <vishaldixitcsit@gmail.com>
2025-09-17 00:17:15 +0530

    changes

--- Basics/searchinrotated.cpp
+++ Basics/searchinrotated.cpp
@@ -1,23 +1,45 @@
 #include <iostream>
 using namespace std;
 
-int searchinrotated(int arr[],int n,int target){
+int Numberofrotate(int arr[],int n){
     int start = 0;
     int end = n-1;
     while(start<=end){
         int mid = start+(end-start)/2;
         int next = (mid+1)%n;
         int pre = (mid+n-1)%n;
-        if(arr[mid]==target) return mid;
+        if(arr[mid]<arr[next] && arr[mid]<arr[pre]) return mid;
         else if(arr[start]<arr[mid]) start = mid + 1;
         else if(arr[mid]<=arr[end]) end = mid -1;
     }
 }
 
+int BinarySearchLeft(int arr[], int l, int r, int target) {
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+
+        if (arr[mid] == target) return mid;
+        else if (arr[mid] < target) l = mid + 1;
+        else r = mid - 1;
+    }
+    return -1;
+}
+
+int BinarySearchRight(int arr[], int l, int r, int target) {
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+
+        if (arr[mid] == target) return mid;
+        else if (arr[mid] < target) l = mid + 1;
+        else r = mid - 1;
+    }
+    return -1;
+}
+
 
 
 int main(){
-   int arr[] = {11,12,15,18,2,5,6,8};
+   int arr[] = {3, 4, 5, 1, 2};
    int n = sizeof(arr) / sizeof(arr[0]);
-   cout<<searchinrotated(arr,n,5);
+   cout<<Numberofrotate(arr,n);
 }
